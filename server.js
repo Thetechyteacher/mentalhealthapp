213542b03627800cae732cc29b64f1c54687d0c7
Monica <117657937+Thetechyteacher@users.noreply.github.com>
2023-03-23 18:21:34 +0000

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = 2121 // this is where I want this app to run locally
 
 let db,
     dbConnectionStr = process.env.DB_STRING, // get the dbconnectionStr from the .env folder
-    dbName = 'to-do'// name of the collection we will be using
+    dbName = 'Cluster0'// name of the collection we will be using
 
 MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true }) // using the parameters in .env, connect to my mongo DB
     .then(client => {
@@ -23,12 +23,12 @@ app.use(express.json())// replaces bodyparser
 
 
 app.get('/',async (request, response)=>{
-    const todoItems = await db.collection('todos').find().toArray()//go get the collection to do and convert it to an array
-    const itemsLeft = await db.collection('todos').countDocuments({completed: false})// get the docs that have the property completed false (the ones left to do)
+    const todoItems = await db.collection('psicohelp').find().toArray()//go get the collection to do and convert it to an array
+    const itemsLeft = await db.collection('psicohelp').countDocuments({completed: false})// get the docs that have the property completed false (the ones left to do)
     response.render('index.ejs', { items: todoItems, left: itemsLeft })//translate the repsonse to the ejs doc y whatever was in to do items, leave it on item, and the ones on itemsLeft where it says left.
-    // db.collection('todos').find().toArray()
+    // db.collection('psicohelp').find().toArray()
     // .then(data => {
-    //     db.collection('todos').countDocuments({completed: false})
+    //     db.collection('psicohelp').countDocuments({completed: false})
     //     .then(itemsLeft => {
     //         response.render('index.ejs', { items: data, left: itemsLeft })
     //     })
@@ -37,7 +37,7 @@ app.get('/',async (request, response)=>{
 })
 
 app.post('/addTodo', (request, response) => { // get the info from /todo form and post it
-    db.collection('todos').insertOne({thing: request.body.todoItem, completed: false})// find the collection todo, insert into the property thing whatever is in the input to-do item
+    db.collection('psicohelp').insertOne({thing: request.body.todoItem, completed: false})// find the collection todo, insert into the property thing whatever is in the input to-do item
     .then(result => {
         console.log('Todo Added')
         response.redirect('/')// once completed, console log and refresh so the info can be sent back to ejs to do its thing on the client side.
@@ -46,7 +46,7 @@ app.post('/addTodo', (request, response) => { // get the info from /todo form an
 })
 
 app.put('/markComplete', (request, response) => {// get the function from the js client side
-    db.collection('todos').updateOne({thing: request.body.itemFromJS},{// find this collection, update one element, insert whatever the body from itemFromJs contains into the property thing.
+    db.collection('psicohelp').updateOne({thing: request.body.itemFromJS},{// find this collection, update one element, insert whatever the body from itemFromJs contains into the property thing.
         $set: {// then set the property completed of this element to true
             completed: true
           }
@@ -63,7 +63,7 @@ app.put('/markComplete', (request, response) => {// get the function from the js
 })
 
 app.put('/markUnComplete', (request, response) => {// get the info sent from the js client side function markuncomplete
-    db.collection('todos').updateOne({thing: request.body.itemFromJS},{// find this collection, update one element, insert whatever the body from itemFromJs contains into the property thing.
+    db.collection('psicohelp').updateOne({thing: request.body.itemFromJS},{// find this collection, update one element, insert whatever the body from itemFromJs contains into the property thing.
         $set: {
             completed: false // then set the property completed of this element to false
           }
@@ -80,7 +80,7 @@ app.put('/markUnComplete', (request, response) => {// get the info sent from the
 })
 
 app.delete('/deleteItem', (request, response) => {
-    db.collection('todos').deleteOne({thing: request.body.itemFromJS})// get the text from itemFromJs
+    db.collection('psicohelp').deleteOne({thing: request.body.itemFromJS})// get the text from itemFromJs
     .then(result => {
         console.log('Todo Deleted')
         response.json('Todo Deleted')
